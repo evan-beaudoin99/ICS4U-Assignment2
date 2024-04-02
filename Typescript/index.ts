402509dcff67c97589cc292bc67a1302181380dd
Evan Beaudoin <124371659+evan-beaudoin99@users.noreply.github.com>
2024-04-02 09:57:34 -0400

    Create index.ts

---
+++ Typescript/index.ts
@@ -0,0 +1,48 @@
+/*
+* This program blows up a string into letters
+*
+* By:      Evan Beaudoin
+* Version: 1.0
+* Since:   2024-03-17
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+function isDigit(char: string): boolean {
+  return !isNaN(parseInt(char));
+}
+
+
+function blowUp(input: string) {
+    let newString = ""
+
+    for (let counter1 = 0; counter1 < input.length; counter1++) {
+        if (isDigit(input[counter1])) { // Checks if value is a digit
+            // Checks if next digit is also a number
+            if (isDigit(input[counter1 + 1])) { 
+              // If yes it ignores the previous number
+              continue 
+            }
+            else {
+              // Multiplies letters by their respected values
+              let mulitplier = parseInt(input[counter1])
+              for (let counter2 = 0; counter2 < mulitplier; counter2++) {
+                // Appends characters to a new string
+                newString += input[counter1 + 1] 
+              }
+          }
+        }
+      else { // If it isn't go to the next value
+        continue 
+      }
+    }
+    return newString
+}
+
+let newString: string = "";
+let inputResult = createPrompt("Enter a String that you want to blow up: ");
+if (inputResult.value !== null) {
+    let input: string = inputResult.value;
+    newString = blowUp(input);
+}
+console.log(newString);
