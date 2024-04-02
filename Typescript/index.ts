9a2da86ebf044ba0f502de1033d3b7d06374a309
Evan Beaudoin <Evan Beaudoin>
2024-04-02 18:04:19 -0400

    Init

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -3,46 +3,45 @@
 *
 * By:      Evan Beaudoin
 * Version: 1.0
-* Since:   2024-03-17
+* Since:   2024-04-02
 */
 
 import { createPrompt } from 'bun-promptx'
 
-function isDigit(char: string): boolean {
-  return !isNaN(parseInt(char));
+function isDigit(char) {
+  return !isNaN(parseInt(char))
 }
 
+function blowUp(input) {
+  let newString = ""
 
-function blowUp(input: string) {
-    let newString = ""
-
-    for (let counter1 = 0; counter1 < input.length; counter1++) {
-        if (isDigit(input[counter1])) { // Checks if value is a digit
-            // Checks if next digit is also a number
-            if (isDigit(input[counter1 + 1])) { 
-              // If yes it ignores the previous number
-              continue 
-            }
-            else {
-              // Multiplies letters by their respected values
-              let mulitplier = parseInt(input[counter1])
-              for (let counter2 = 0; counter2 < mulitplier; counter2++) {
-                // Appends characters to a new string
-                newString += input[counter1 + 1] 
-              }
-          }
-        }
-      else { // If it isn't go to the next value
+  for (let counter1 = 0; counter1 < input.length; counter1++) {
+    if (isDigit(input[counter1])) { // Checks if value is a digit
+      // Checks if next digit is also a number
+      if (isDigit(input[counter1 + 1])) { 
+        // If yes it ignores the previous number
         continue 
       }
+      else {
+        // Multiplies letters by their respected values
+        let mulitplier = parseInt(input[counter1])
+        for (let counter2 = 0; counter2 < mulitplier; counter2++) {
+          // Appends characters to a new string
+          newString += input[counter1 + 1] 
+        }
+      }
     }
-    return newString
+    else { // If it isn't go to the next value
+      continue 
+    }
+  }
+  return newString
 }
 
-let newString: string = "";
-let inputResult = createPrompt("Enter a String that you want to blow up: ");
+let newString = ""
+let inputResult = createPrompt("Enter a String that you want to blow up: ")
 if (inputResult.value !== null) {
-    let input: string = inputResult.value;
-    newString = blowUp(input);
+  let input = inputResult.value
+  newString = blowUp(input)
 }
-console.log(newString);
+console.log(newString)
